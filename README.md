56771de78dc9b8161dfe03043e791d8022b21b67
Dawid Stach <dstach20@gmail.com>
2022-07-08 14:05:55 +0200

    Create canvas backgraund and draw stars on sky

---
+++ README.md
@@ -0,0 +1,10 @@
+
+<h1>Application for viewing photos of all breeds of dogs !!</h1>
+
+App using https://dog.ceo/ API and Minimalist Webpack Boilerplate
+
+
+
+
+![image](https://user-images.githubusercontent.com/98091205/176891995-0c31dd66-190a-4db7-b3c4-240e605cdfb5.png)
+
---
+++ src/js/index.js
@@ -0,0 +1,97 @@
+import '../sass/index.scss';
+/** @type {HTMLCanvasElement} */
+
+class Sky {
+    constructor(canvas) {
+        this.canvas = canvas;
+        this.ctx = canvas.getContext('2d');
+        this.width = window.innerWidth;
+        this.height = window.innerHeight;
+        this.startX = 0;
+        this.startY = 0;
+        this.stars = [];
+    }
+
+    generateStars(count) {
+        let stars = [];
+
+        for (let i = 0; i < count; i++) {
+            const radius = Math.random() * 3 + 2;
+
+            stars.push({
+                x: Math.random() * this.width,
+                y: Math.random() * this.height,
+                radius: radius,
+                color: 'rgba(255, 255, 255, 1)',
+                speed: Math.random() * 0.3,
+            });
+        }
+
+        this.stars = stars;
+    }
+
+    clearCanvas() {
+        this.ctx.fillStyle = 'black';
+        this.ctx.fillRect(this.startX, this.startY, this.width, this.height);
+    }
+
+    updateStar() {
+        this.stars.forEach((star) => {
+            star.x += star.speed;
+            star.y -= (star.speed * (this.width / 2 - star.x)) / 3000;
+
+            if (star.x > this.width + 2 * star.radius) {
+                star.x = star.radius * -2;
+            }
+        });
+    }
+
+    drowStars() {
+        this.stars.forEach((star) => {
+            this.drawStar(star);
+        });
+    }
+
+    drawStar(star) {
+        this.ctx.save();
+
+        this.ctx.fillStyle = star.color;
+
+        this.ctx.beginPath();
+        this.ctx.translate(star.x, star.y);
+        this.ctx.moveTo(0, 0 - star.radius);
+
+        for (let i = 0; i < 5; i++) {
+            this.ctx.rotate((Math.PI / 180) * 36);
+            this.ctx.lineTo(0, 0 - star.radius * 0.6);
+            this.ctx.rotate((Math.PI / 180) * 36);
+            this.ctx.lineTo(0, 0 - star.radius);
+        }
+
+        this.ctx.fill();
+        this.ctx.restore();
+    }
+
+    draw() {
+        this.clearCanvas();
+        this.updateStar();
+        this.drowStars();
+        window.requestAnimationFrame(() => this.draw());
+    }
+
+    initCanvas() {
+        this.canvas.width = this.width;
+        this.canvas.height = this.height;
+
+        this.clearCanvas();
+    }
+
+    run() {
+        this.initCanvas();
+        this.generateStars(700);
+        this.draw();
+    }
+}
+
+const sky = new Sky(document.querySelector('#canvas'));
+sky.run();
